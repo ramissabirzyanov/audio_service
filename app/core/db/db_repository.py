b22031a284e9b1a6808034d2e852c390579c3eb7
Ramis <ramis@Ramiss-MacBook-Air.local>
2025-04-01 13:10:36 +0300

    add audio repository

--- app/core/db/db_repository.py
+++ app/core/db/db_repository.py
@@ -6,6 +6,7 @@ from sqlalchemy import update, delete
 from sqlalchemy.orm import selectinload
 
 from app.core.models.user import User
+from app.core.models.audio_file import AudioFile
 
 
 class UserRepository:
@@ -42,21 +43,63 @@ class UserRepository:
         )
         await self.db.commit()
 
-    async def get_user_by_id(self, id: int) -> User:
-        result = await self.db.execute(select(User).where(User.id == id))
+    async def get_user_by_id(self, user_id: int) -> User:
+        result = await self.db.execute(select(User).where(User.id == user_id))
         return result.scalars().first()
     
-    async def make_super_user(self, id: int) -> None:
+    async def make_super_user(self, user_id: int) -> None:
         await self.db.execute(
             update(User)
-            .where(User.id == id)
+            .where(User.id == user_id)
             .values(is_superuser=True)
         )
         await self.db.commit()
 
-    async def delete_user(self, id: int) -> None:
+    async def delete_user(self, user_id: int) -> None:
         await self.db.execute(
             delete(User)
-            .where(User.id == id)
+            .where(User.id == user_id)
+        )
+        await self.db.commit()
+
+
+class AudioRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def add_audio_file(
+        self,
+        user_id: int,
+        filename: str,
+        file_path: str
+    ) -> AudioFile:
+        """
+        Добавление аудио файла.
+        """
+        audio = AudioFile(
+            user_id=user_id,
+            filename=filename,
+            path=file_path
+        )
+        self.db.add(audio)
+        await self.db.commit()
+        await self.db.refresh(audio)
+        return audio
+    
+    async def update_audio_file(self, audi_data, audio_id):
+        await self.db.execute(
+            update(AudioFile)
+            .where(AudioFile.id == audio_id)
+            .values(
+                filename=audi_data['filename'],
+                path=audi_data['path']
+            )
+        )
+        await self.db.commit()
+
+    async def delete_audio_file(self, audio_id: int) -> None:
+        await self.db.execute(
+            delete(AudioFile)
+            .where(AudioFile.id == audio_id)
         )
         await self.db.commit()
